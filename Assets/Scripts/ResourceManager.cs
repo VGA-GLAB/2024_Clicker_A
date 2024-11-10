2471c068b3204aa90d97be14a0a3a434abff3595
KasaharaSota <souta.programer1201@gmail.com>
2024-11-10 15:13:38 +0900

    [Update]カーソルだけ10秒に一回リソースを増やすようにして、カーソル1つあたりの生産量を0.1リソース/秒にする。あと小数点を扱える構造体の開発中。

--- Assets/Scripts/ResourceManager.cs
+++ Assets/Scripts/ResourceManager.cs
@@ -28,6 +28,52 @@ public class ResourceManager : MonoBehaviour
         [Tooltip("購入可能かどうか")] public bool CanBuy;
         [Tooltip("価格を表示するテキスト")] public TextMeshProUGUI PriceText;
     }
+    //小数点以下も扱える構造体の開発中
+    [Serializable]
+    public struct BigNumber
+    {
+        public int 何桁区切りか;
+        public List<単位> NumberList;
+        public void IncreaseNumber(BigNumber number)
+        {
+            for (int i = 0; i < NumberList.Count; i++)
+            {
+                NumberList[i].Number += number.NumberList[i].Number;
+            }
+            繰り上げ();
+        }
+        void 繰り上げ()
+        {
+            for (int i = 0; i < NumberList.Count - 1; i++)
+            {
+                NumberList[i + 1].Number += (int)(NumberList[i].Number / Mathf.Pow(10, 何桁区切りか - 1));
+            }
+        }
+        void DecreaseNumber(BigNumber number)
+        {
+            for (int i = 0; i < NumberList.Count; i++)
+            {
+                NumberList[i].Number -= number.NumberList[i].Number;
+            }
+            繰り下げ();
+        }
+        void 繰り下げ()
+        {
+            for (int i = 0; i < NumberList.Count - 1; i++)
+            {
+                if(NumberList[i].Number < 0)
+                {
+                    NumberList[i].Number += (int)Mathf.Pow(10, 何桁区切りか);
+                    NumberList[i + 1].Number -= 1;
+                }
+            }
+        }
+    }
+    public class 単位
+    {
+        public string Name;
+        public int Number;
+    }
     void Start()
     {
         for (int i = 0; i < _products.Count; i++)
@@ -44,12 +90,12 @@ public class ResourceManager : MonoBehaviour
     /// 毎秒リソースを増やす
     /// </summary>
     /// <returns></returns>
-    IEnumerator GainPerSecond(Product p)
+    IEnumerator GainPerSecond(Product p, int interval)
     {
         while (true)
         {
             IncreaseResource(p.ResourcePerSecond);
-            yield return new WaitForSeconds(1);
+            yield return new WaitForSeconds(interval);
         }
     }
     /// <summary>
@@ -91,7 +137,10 @@ public class ResourceManager : MonoBehaviour
             p.CanBuy = p.Price <= _resource;
             if (p.UnitCount == 1)
             {
-                StartCoroutine(GainPerSecond(p));
+                if(p.Name != "Cursor")
+                StartCoroutine(GainPerSecond(p , 1));
+                else
+                StartCoroutine(GainPerSecond(p, 10));
             }
             p.PriceText.text = $"{p.Name}:{p.Price}";
         }
