2240fc526cf6db837376f138ed96ae90f6c6d8e6
KRYG <abcdef0509a@gmail.com>
2024-12-02 16:06:47 +0900

    [update]ボスをクリックしたときの処理を追加

--- Assets/Scripts/MiniGameQuestsManager.cs
+++ Assets/Scripts/MiniGameQuestsManager.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.UIElements;
 using Random = UnityEngine.Random;
 
 public class MiniGameQuestsManager : MonoBehaviour
@@ -11,13 +12,21 @@ public class MiniGameQuestsManager : MonoBehaviour
     [SerializeField] private List<BossParameter> _bossParameters;
     [SerializeField] private TextMeshProUGUI _timerText;
     [SerializeField] private Image _hpGauge;
+    /// <summary>
+    /// ダメージポップアップ
+    /// </summary>
+    [SerializeField] private TextMeshProUGUI _damegeText;
+    /// <summary>
+    /// ダメージテキストの表示時間
+    /// </summary>
+    [SerializeField] private int _textLifeTime;
     private int _currentHP;
     private float _timeLimit;
     /// <summary>
-    /// クリックで与えたそうダメージ
+    /// クリックで与えた総ダメージ
     /// </summary>
     private int _damageTotal;
-    [Header("1クリックあたりの与ダメージ量"),SerializeField] private int _clickDamage;
+    [Header("1クリックあたりの与ダメージ量"), SerializeField] private int _clickDamage;
     private int _clickLevel;
     /// <summary>
     /// １秒あたりに与える自動ダメージ
@@ -27,6 +36,11 @@ public class MiniGameQuestsManager : MonoBehaviour
     /// 現在のボスのID
     /// </summary>
     private int _currentBossId;
+    /// <summary>
+    /// リザルトを表示するパネル
+    /// </summary>
+    [SerializeField] GameObject _resultPanel;
+
 
     public class BossParameter : ScriptableObject
     {
@@ -36,6 +50,7 @@ public class MiniGameQuestsManager : MonoBehaviour
     }
     private void Start()
     {
+        _resultPanel.SetActive(false);
         _currentBossId = Random.Range(0, 4);
         _currentHP = _bossParameters[_currentBossId].BossMaxHP;
     }
@@ -46,6 +61,23 @@ public class MiniGameQuestsManager : MonoBehaviour
         {
             Timer();
         }
+        else
+        {
+            _resultPanel.SetActive(true);
+        }
+    }
+
+    /// <summary>
+    /// 敵をクリックしたときダメージを与える処理
+    /// </summary>
+    public void ClickDamage()
+    {
+        _currentHP -= _clickDamage;
+        _damageTotal += _clickDamage;
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePos.z = 0;
+        TextMeshProUGUI text = Instantiate(_damegeText, mousePos, Quaternion.identity);//クリックした場所にテキストを表示
+        StartCoroutine(DestroyTextAfterTime(text, _textLifeTime));//上で表示したテキストを_textLifeTime秒後に消す　
     }
 
     private void Timer()
@@ -54,9 +86,22 @@ public class MiniGameQuestsManager : MonoBehaviour
         _timerText.text = _timeLimit.ToString("0.00");
     }
 
+    /// <summary>
+    /// ボスのHP表示を更新する処理
+    /// </summary>
+    /// <param name="n"></param>
     private void BossHP(int n)
     {
         BossParameter b = _bossParameters.Find(b => b.Id == _currentBossId);
         _hpGauge.fillAmount = _currentHP / b.BossMaxHP;
+    }
+
+    /// <summary>
+    /// 数秒後にテキストを消す処理
+    /// </summary>
+    private IEnumerator DestroyTextAfterTime(TextMeshProUGUI textObject, float time)
+    {
+        yield return new WaitForSeconds(time);
+        Destroy(textObject);  // テキストを削除
     }
 }
