864877f1243dd8a1f4e6f025ccbbcb505838cd35
KRYG <abcdef0509a@gmail.com>
2024-12-08 17:13:22 +0900

    [update]playTimerを修正

--- Assets/Scripts/PlayTimer.cs
+++ Assets/Scripts/PlayTimer.cs
@@ -6,21 +6,21 @@ using UnityEngine;
 public class PlayTimer : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI _timerText;
-    private float _hour = 0;
-    private float _minute = 0;
+    private int _hour = 0;
+    private int _minute = 0;
     private float _second = 0;
     void Update()
     {
         _second += Time.deltaTime;
         if (_second >= 60)
         {
-            _second = 0;
-            _minute += 1;
-        }
-        if (_minute >= 60)
-        {
-            _minute = 0;
-            _hour += 1;
+            _second -= 60;
+            _minute++;
+            if (_minute >= 60)
+            {
+                _minute = 0;
+                _hour++;
+            }
         }
         _timerText.text = $"PlayTime{_hour.ToString("00")}:{_minute.ToString("00")}:{_second.ToString("00")}";
     }
