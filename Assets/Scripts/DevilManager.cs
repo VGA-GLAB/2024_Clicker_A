a95783cf20c6142e79d69307d385025f4e0ccb4a
Royzn <yamiryo22@gmail.com>
2024-12-12 10:42:35 +0900

    [update]コメント内容修正しました

--- Assets/Scripts/DevilManager.cs
+++ Assets/Scripts/DevilManager.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class DevilManager : MonoBehaviour
 {
     [System.Serializable]
-    public class Devils
+    public class Devil
     {
         public string _Name;  //使い魔の名前
         public bool _isActive;　　//アクティブ状態
@@ -20,7 +20,7 @@ public class DevilManager : MonoBehaviour
         public float _levelupAutoClickPower;　//レベルアップごとの自動クリック上昇
 
 
-        public Devils(string _name, RarityType rarity, float productivity, float interval)
+        public Devil(string _name, RarityType rarity, float productivity, float interval)
         {
             _Name = _name;
             _isActive = false;
@@ -34,10 +34,26 @@ public class DevilManager : MonoBehaviour
             _levelupAutoClickPower = 0;
         }
     }
-    public enum RarityType { Normal, Rare, SRare, Legend}
-    public enum Skills { FlashDamage, AutoDamage, TimeDamage, FlashDmUp, AutoDmUp, StopTime, ChangeClick, ChangeAutoClick}
+    public enum RarityType 
+    { 
+        Normal, 
+        Rare, 
+        SRare,
+        Legend
+    }
+    public enum Skills 
+    { 
+        FlashDamage,     //瞬間ダメージ
+        AutoDamage,      //自動ダメージ
+        TimeDamage,      //時間ダメージ
+        FlashDmUp,       //瞬時クリック力増加
+        AutoDmUp,        //瞬間自動クリック増加
+        StopTime,        //時間増加
+        ChangeClick,     //クリック力変換
+        ChangeAutoClick  //自動クリック変換
+    }
 
-    public List<Devils> _devils = new List<Devils>();
+    public List<Devil> _devils = new List<Devil>();
     public int _maxDevils = 6;
 
 
