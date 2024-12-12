ddc87f3790ccae342a6fb5f563cd7db6327e8244
KRYG <abcdef0509a@gmail.com>
2024-12-13 01:03:42 +0900

    [update]使い魔の効果を適用、使い魔のスキルの要素を追加、SkillButton.csの内容を参照を減らすためMiniGameQuestsManager.csに移動

--- Assets/Scripts/MiniGameQuestsManager.cs
+++ Assets/Scripts/MiniGameQuestsManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using static DevilManager;
 using Random = UnityEngine.Random;
 
 public class MiniGameQuestsManager : MonoBehaviour
@@ -15,6 +16,9 @@ public class MiniGameQuestsManager : MonoBehaviour
     [Header("リザルトで表示する評価"), SerializeField] private TextMeshProUGUI _resultScore;
     [SerializeField] private Image _hpGauge;
     [SerializeField] private Canvas _canvas;
+    [Header("スキルボタンをすべて入れる"), SerializeField] private Button[] _skillBottons;
+    [Header("各ボタンのテキスト"), SerializeField] private TextMeshProUGUI[] _buttonTexts;
+    [Header("発動したスキルを表示"), SerializeField] private TextMeshProUGUI _skillText;
     /// <summary>
     /// ダメージポップアップ
     /// </summary>
@@ -24,17 +28,17 @@ public class MiniGameQuestsManager : MonoBehaviour
     /// </summary>
     [SerializeField] private int _textLifeTime;
     private float _currentHP;
-    public float TimeLimit;
+    [SerializeField] private float _timeLimit;
     /// <summary>
     /// クリックで与えた総ダメージ
     /// </summary>
     private float _damageTotal;
-    [Header("1クリックあたりの与ダメージ量"), SerializeField] private int _clickDamage;
+    [Header("1クリックあたりの与ダメージ量"), SerializeField] private float _clickDamage;
     private int _clickLevel;
     /// <summary>
     /// １秒あたりに与える自動ダメージ
     /// </summary>
-    private int _autoDamage;
+    private float _autoDamage;
     /// <summary>
     /// 現在のボスのID
     /// </summary>
@@ -44,7 +48,9 @@ public class MiniGameQuestsManager : MonoBehaviour
     /// </summary>
     [SerializeField] GameObject _resultPanel;
 
-    List<GameObject> _devilList;
+    DevilManager _devilManager;
+
+    List<Devil> _devilList = new List<Devil>();
 
     private bool _isTimeStop = false;
 
@@ -61,7 +67,20 @@ public class MiniGameQuestsManager : MonoBehaviour
         _currentBossId = Random.Range(0, 4);
         _currentHP = _bossParameters[_currentBossId].BossMaxHP;
         _clickPowerText.text = $"Power : {_clickDamage.ToString()}";
+        _devilManager = FindAnyObjectByType<DevilManager>();
         StartCoroutine(AutoDamage());
+        Debug.Log(_devilManager._devils[_devilManager._devils.Count - 1]._Name);
+
+        for (int i = 0; i < _devilManager._devils.Count; i++)
+        {
+            Debug.Log(i);
+            _devilList.Add(_devilManager._devils[i]);//ここでAddするリストは実際とは異なり使い魔編成のシステムが追加され次第変更します
+            _autoDamage += _devilList[i]._autoClickPower;
+            _clickDamage += _devilList[i]._clickPower;
+        }
+        StartCoroutine(ButtonEnabled());
+
+        _skillText.text = "";
     }
 
     private void Update()
@@ -72,6 +91,16 @@ public class MiniGameQuestsManager : MonoBehaviour
             Debug.Log(_currentBossId);
             Debug.Log(_currentHP);
         }//デバック用
+
+        if (_timeLimit <= 0)
+        {
+            for (int i = 0; i < _devilList.Count; i++)
+            {
+                _skillBottons[i].enabled = false;
+                _skillBottons[i].image.color = new Color(0, 0, 0, 0);
+                _skillBottons[i].GetComponentInChildren<TextMeshProUGUI>().text = "";
+            }
+        }//タイマーが0になったらボタンを全て消す
     }
 
     private void SkillDamage(float damage)
@@ -103,16 +132,17 @@ public class MiniGameQuestsManager : MonoBehaviour
 
     private void Timer()
     {
-        if (TimeLimit > 0)
+        if (_timeLimit > 0)
         {
-            TimeLimit -= Time.deltaTime;
+            _timeLimit -= Time.deltaTime;
             if (!_isTimeStop)
             {
-                _timerText.text = TimeLimit.ToString("0.00");
+                _timerText.text = _timeLimit.ToString("0.00");
             }
         }
         else
         {
+            _skillText.text = "";
             _resultPanel.SetActive(true);
             _resultHP.text = $"BossHP : {_currentHP.ToString("00000000")}";
             _resultScore.text = $"Score : {ResultScore()}";
@@ -154,9 +184,63 @@ public class MiniGameQuestsManager : MonoBehaviour
         return score;
     }
 
+    /// <summary>
+    /// スキルボタンに表示されたテキストからスキルを判定する
+    /// </summary>
+    public void Skill(TextMeshProUGUI text)
+    {
+
+        if (text.text == "Skill Failed")
+        {
+            _skillText.text = "Skill Failed";
+            return;
+        }
+
+        Devil devil = _devilList.Find(devil => devil._Name == text.text);
+        text.text = "";
+        Debug.Log(devil._Name);
+        switch (devil._skill)
+        {
+            case Skills.FlashDamage:
+                SkillDamage(_clickDamage * (2 + devil._level));
+                _skillText.text = $"{devil._Name}:FlashDamage";
+                break; //瞬時ダメージ
+            case Skills.AutoDamage:
+                SkillDamage(_autoDamage * (5 + devil._level));
+                _skillText.text = $"{devil._Name}:AutoDamage";
+                break;
+            case Skills.TimeDamage:
+                SkillDamage(_timeLimit * (2 + devil._level));
+                _skillText.text = $"{devil._Name}:TimeDamage";
+                break;
+            case Skills.FlashDmUp:
+                _clickDamage += devil._clickPower * devil._level;
+                _skillText.text = $"{devil._Name}:FlashDamageUP";
+                break;
+            case Skills.AutoDmUp:
+                _autoDamage += devil._autoClickPower * devil._level;
+                _skillText.text = $"{devil._Name}:AutoDamageUP";
+                break;
+            case Skills.StopTime:
+                StartCoroutine(TimeStopper());
+                _skillText.text = $"{devil._Name}:StopTime";
+                break;
+            case Skills.ChangeClick:
+                _clickDamage += _autoDamage;
+                _skillText.text = $"{devil._Name}:ChangeClickPower";
+                _autoDamage = 0;
+                break;
+            case Skills.ChangeAutoClick:
+                _autoDamage += _clickDamage;
+                _clickDamage = 0;
+                _skillText.text = $"{devil._Name}:ChangeAutoPower";
+                break;
+        }
+    }
+
     IEnumerator AutoDamage()
     {
-        while (TimeLimit >= 0)
+        while (_timeLimit >= 0)
         {
             yield return new WaitForSeconds(1);
             _currentHP -= _autoDamage;
@@ -165,12 +249,82 @@ public class MiniGameQuestsManager : MonoBehaviour
         }
     }
 
-    /// <summary>
-    /// ボタンに数字を設定し使い魔のスキルとボタンを紐づける
-    /// </summary>
-    /// <param buttonNumber="n"></param>
-    public void Skill(int n)
+    public void ButtonDelete()
     {
+        this.enabled = false;
+    }
 
+    /// <summary>
+    /// 時間を止める処理
+    /// </summary>
+    /// <returns></returns>
+    IEnumerator TimeStopper()
+    {
+        _timeLimit++;
+        _isTimeStop = true;
+        yield return new WaitForSeconds(1);
+        _isTimeStop = false;
+    }
+
+    /// <summary>
+    /// 一秒に一回スキルボタンが押せるか抽選をする処理
+    /// </summary>
+    /// <returns></returns>
+    IEnumerator ButtonEnabled()
+    {
+        while (_timeLimit > 0)
+        {
+            for (int i = 0; i < 6; i++)
+            {
+                _skillBottons[i].enabled = false;
+                _skillBottons[i].image.color = Color.gray;
+                _buttonTexts[i].text = "";
+            }
+
+            for (int i = 0; i < 6; i++)
+            {
+                var number = Random.Range(1, 10);
+                if (number == 1)
+                {
+                    _skillBottons[i].enabled = true;
+                    _skillBottons[i].image.color = Color.red;
+                    var n = Random.Range(1, 7);
+                    switch (n)
+                    {
+                        case 0:
+                            _buttonTexts[i].text = _devilList[0]._Name;
+                            Debug.Log(_devilList[0]._Name);
+                            break;
+                        case 1:
+                            _buttonTexts[i].text = _devilList[1]._Name;
+                            Debug.Log (_devilList[1]._Name);
+                            break;
+                        case 2:
+                            _buttonTexts[i].text = _devilList[2]._Name;
+                            Debug.Log ( _devilList[2]._Name);
+                            break;
+                        case 3:
+                            _buttonTexts[i].text = _devilList[3]._Name;
+                            break;
+                        case 4:
+                            _buttonTexts[i].text = _devilList[4]._Name;
+                            break;
+                        case 5:
+                            _buttonTexts[i].text = _devilList[5]._Name;
+                            break;
+                        case 6:
+                            _buttonTexts[i].text = "Skill Failed";
+                            break;
+                    }//ボタンに振られたスキルが何か表示する
+                }
+            }
+            if (_timeLimit <= 0)
+            {
+                yield break;
+            }
+
+            yield return new WaitForSeconds(1f);
+        }
     }
 }
+
