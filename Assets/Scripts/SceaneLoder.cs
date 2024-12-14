7868a98cce791f7b9dd0b2f176c44bd9fb8aa1bf
KRYG <abcdef0509a@gmail.com>
2024-12-14 23:29:46 +0900

    [add]メインゲームとミニゲームのシーンを作成し、シーンを切り替える処理を追加

---
+++ Assets/Scripts/SceaneLoder.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class SceaneLoder : MonoBehaviour
+{
+    [SerializeField] SceneName _sceneName;
+    public enum SceneName
+    {
+        MainGame,
+        MiniGame
+    }
+    public void SceaneLoader()
+    {
+        Debug.Log(_sceneName.ToString());
+        SceneManager.LoadScene(_sceneName.ToString());
+    }
+}
